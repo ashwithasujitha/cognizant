43547b9fd94396a0cf4272bf88a922d81bd3f082
ashwithasujitha <150882281+ashwithasujitha@users.noreply.github.com>
2026-07-25 22:59:01 +0530

    Delete studentapp directory

--- studentapp/src/component/Contact.jsx
+++
@@ -1,7 +0,0 @@
-function Contact()
-{
-    return(
-        <h1>Welcome to the contact Page of Student Management Portal</h1>
-    )
-}
-export default Contact;
--- studentapp/src/component/Home.jsx
+++
@@ -1,7 +0,0 @@
-function Home()
-{
-    return (
-        <h1>Welcome to the Home Page of Student Management Portal</h1>
-    )
-}
-export default Home;
